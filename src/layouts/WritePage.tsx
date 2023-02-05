d1d9f04004a090a3a1d67b30fcf8a6af0889bbcd
GyoungMin <101560583+Bae0203@users.noreply.github.com>
2023-02-05 10:08:47 +0900

    ADD :: 사진 올리기
    해야할 것
    - 프로필 사진 올리기 구현하기
    - 그림 올리기 구현하기

--- src/layouts/WritePage.tsx
+++ src/layouts/WritePage.tsx
@@ -1,4 +1,47 @@
+import { useRef, useState, useMemo } from "react";
+import BlankImg from "../asset/person.png";
+
+type UploadImage = {
+  file: File;
+  thumbnail: string;
+  type: string;
+};
+
 export default function WritePage() {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const [img, setImg] = useState<UploadImage | null>(null);
+  const handleClickFileInput = () => {
+    fileInputRef.current?.click();
+  };
+
+  const uploadFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const fileList = e.target.files;
+    const length = fileList?.length;
+    if (fileList && fileList[0]) {
+      const url = URL.createObjectURL(fileList[0]);
+
+      setImg({
+        file: fileList[0],
+        thumbnail: url,
+        type: fileList[0].type.slice(0, 5),
+      });
+      console.log("d", url);
+    }
+  };
+
+  const showImage = useMemo(() => {
+    if (!img && img == null) {
+      return <img src={BlankImg} alt="사진이 비어있습니다." />;
+    }
+    return (
+      <img
+        src={img.thumbnail}
+        alt={img.type}
+        onClick={handleClickFileInput}
+      ></img>
+    );
+  }, [img]);
+
   return (
     <>
       <nav>
@@ -9,9 +52,16 @@ export default function WritePage() {
         <label>내용</label>
         <textarea placeholder="내용을 입력해주세요."></textarea>
       </nav>
-      <nav>
-        <input type="file" accept="image/*" />
-      </nav>
+      {showImage}
+      <form>
+        <input
+          type="file"
+          accept="image/*"
+          ref={fileInputRef}
+          onChange={uploadFile}
+        />
+        <input type="button" onClick={handleClickFileInput} />
+      </form>
     </>
   );
 }
