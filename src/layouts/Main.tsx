7da7ce83f22ac6691758904a080138929e0d1d62
bae0203 <gmbae06@dgsw.hs.kr>
2023-03-08 21:52:35 +0900

    ADD :: Get Set LocalStorage

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,7 +1,36 @@
+import { useEffect, useState } from "react";
+import PostContent from "../components/PostContent";
+
 function Main() {
+  const [titleList, setTitleList] = useState<string[]>([]);
+  const [contextList, setContextList] = useState<string[]>([]);
+  const [imageUrlList, setImageUrlList] = useState<string[]>([]);
+
+  useEffect(() => {
+    let parseTitle: string | null = localStorage.getItem("title");
+    let parseContext: string | null = localStorage.getItem("context");
+    let parseImageUrl: string | null = localStorage.getItem("image");
+    if (parseTitle) {
+      const TitleList: string[] = JSON.parse(parseTitle);
+      setTitleList([...TitleList]);
+    }
+    if (parseContext) {
+      const ContextList: string[] = JSON.parse(parseContext);
+      setContextList([...ContextList]);
+    }
+    if (parseImageUrl) {
+      const ImageUrlList: string[] = JSON.parse(parseImageUrl);
+      setImageUrlList([...ImageUrlList]);
+    }
+  }, []);
+
   return (
     <>
       <p>내 글 목록들</p>
+      <button onClick={() => localStorage.clear()}>dd</button>
+      {titleList.map((e, i) => {
+        return <PostContent title={e} context={contextList[i]}></PostContent>;
+      })}
     </>
   );
 }
--- src/layouts/WritePage.tsx
+++ src/layouts/WritePage.tsx
@@ -1,10 +1,51 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import UploadImage from "../components/UploadImage";
 
 const WritePage = () => {
-  const [newImageUrl, setNewImageUrl] = useState<string | null>(null);
+  const Navigate = useNavigate();
+
   const [newTitle, setNewTitle] = useState<string>("");
   const [newContext, setNewContext] = useState<string>("");
+  const [newImageUrl, setNewImageUrl] = useState<string | null>(null);
+
+  const SaveInformationCheck = () => {
+    let parseTitle: string | null = localStorage.getItem("title");
+    let parseContext: string | null = localStorage.getItem("context");
+    let parseImageUrl: string | null = localStorage.getItem("image");
+
+    if (parseTitle) {
+      let TitleList: string[] = JSON.parse(parseTitle);
+      TitleList.push(newTitle);
+      localStorage.setItem("title", JSON.stringify(TitleList));
+    } else {
+      let TitleList: string[] = [newTitle];
+      localStorage.setItem("title", JSON.stringify(TitleList));
+    }
+    if (parseContext) {
+      let ContextList: string[] = JSON.parse(parseContext);
+      ContextList.push(newContext);
+      localStorage.setItem("context", JSON.stringify(ContextList));
+    } else {
+      let ContextList: string[] = [newContext];
+      localStorage.setItem("context", JSON.stringify(ContextList));
+    }
+    if (parseImageUrl) {
+      if (newImageUrl != null) {
+        let ImageUrlList: string[] = JSON.parse(parseImageUrl);
+        ImageUrlList.push(newImageUrl);
+        localStorage.setItem("image", JSON.stringify(ImageUrlList));
+      }
+    } else {
+      if (newImageUrl != null) {
+        let ImageUrlList: string[] = [newImageUrl];
+        localStorage.setItem("image", JSON.stringify(ImageUrlList));
+      }
+    }
+
+    alert("생성되었습니다!");
+    Navigate("/");
+  };
 
   const SaveButtonHandler = () => {
     let CheckSaveContent = window.confirm("게시물을 올리겠습니까?");
@@ -12,6 +53,8 @@ const WritePage = () => {
       if (newImageUrl == null || newTitle == null || newContext == null) {
         alert("정보를 제대로 입력해주세요");
         return;
+      } else {
+        SaveInformationCheck();
       }
     }
   };
