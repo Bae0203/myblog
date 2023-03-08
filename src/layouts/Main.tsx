badbff310c532ef7097e9ebfd1b4776f5a1186ad
bae0203 <gmbae06@dgsw.hs.kr>
2023-03-08 19:58:23 +0900

    FIX :: Main Form

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,30 +1,7 @@
-import { useEffect, useState } from "react";
-import PostBox from "../components/PostBox";
-
-interface IContent {
-  title: string;
-  context: string;
-}
-
 function Main() {
-  const [content, setContent] = useState<IContent[] | null>();
-  useEffect(() => {
-    const TitleList: string | null = localStorage.getItem("title");
-    const ContextList: string | null = localStorage.getItem("context");
-    if (TitleList != null && ContextList != null) {
-      let ParseTitleList: string[] = JSON.parse(TitleList);
-      let ParseContextList: string[] = JSON.parse(ContextList);
-      ParseTitleList.map((value, index) => {
-        let CombineContent: IContent = {
-          title: value,
-          context: ParseContextList[index],
-        };
-      });
-    }
-  }, []);
   return (
     <>
-      <PostBox></PostBox>
+      <p>내 글 목록들</p>
     </>
   );
 }
