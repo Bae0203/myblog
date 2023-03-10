69e85da129c93dd6610b06304fdc8ee79740593c
GyoungMin <101560583+Bae0203@users.noreply.github.com>
2023-03-10 22:53:38 +0900

    FIX :: useParams
    참고
    https://velog.io/@gmbae2006/useParams-%EC%82%AC%EC%9A%A9%EA%B8%B0

--- src/layouts/DetailPage.tsx
+++ src/layouts/DetailPage.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import { EntriePostContent, IPostContent } from "../store/PostInformation";
+import { useRecoilValue } from "recoil";
 
 const DetailPage = () => {
-  const { params } = useParams();
+  const { id } = useParams();
+  const PostValue = useRecoilValue<IPostContent | null>(EntriePostContent);
   return (
     <>
-      <div>dd</div>
-      <p>제목 : </p>
-      <p>내용 : </p>
-      <img src="" alt="image" />
+      {PostValue && id ? (
+        <>
+          <div>dd</div>
+          <button
+            onClick={() => console.log(PostValue.title[Number(id)], Number(id))}
+          >
+            ddd
+          </button>
+          <p>제목 : {PostValue.title[Number(id)]}</p>
+          <p>내용 : </p>
+          <img src="" alt="image" />
+        </>
+      ) : (
+        <p>404! 내용이 없습니다!</p>
+      )}
     </>
   );
 };
